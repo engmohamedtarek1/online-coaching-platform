9bb37e7a3e2eebe1c7f2c44c97a7cf0a0f5a3962
engmohamedtarek1 <eng.mohamedtarek0@gmail.com>
2024-03-08 08:40:08 +0200

    Change Layout and Home page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,62 +3,68 @@ import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+	{
+		title: 'Nutrition',
+		Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+		pic: 'https://cdn.fs.teachablecdn.com/ZVtblpzQMuz66985Ls59',
+		description: (
+			<>
+				We design personalized dietary plans based on your current goals and
+				your preferences for favorite foods. Learn about calorie and how to
+				adjust your daily food choices accordingly.
+			</>
+		),
+	},
+	{
+		title: 'Training System',
+		Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+		pic: 'https://cdn.fs.teachablecdn.com/3FalutE5SMnIcYLTz2AQ',
+		description: (
+			<>
+				We tailor a comprehensive training program to the number of available
+				days per week. This detailed system includes selected exercises and
+				workout intensity, based on the equipment available.
+			</>
+		),
+	},
+	{
+		title: 'Follow-Up',
+		Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+		pic: 'https://cdn.fs.teachablecdn.com/oNnspThRRjyxdaVC2hew',
+		description: (
+			<>
+				Get assessments on your calorie progress and corrections. Evaluate your
+				exercise performance and intensity through videos, and receive
+				explanations and clarifications on aspects of the program.
+			</>
+		),
+	},
 ];
 
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ title, description, pic }) {
+	return (
+		<div className={clsx('col col--4')}>
+			<div className="text--center">
+				<img src={pic} height="300" />
+			</div>
+			<div className="text--center padding-horiz--md">
+				<Heading as="h3">{title}</Heading>
+				<p>{description}</p>
+			</div>
+		</div>
+	);
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.features}>
+			<div className="container">
+				<div className="row">
+					{FeatureList.map((props, idx) => (
+						<Feature key={idx} {...props} />
+					))}
+				</div>
+			</div>
+		</section>
+	);
 }
